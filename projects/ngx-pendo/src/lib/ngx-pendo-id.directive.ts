c2d3c4c491049bfcbe759b04cb879bb7c1f045e5
Eden Wang <yociduo@vip.qq.com>
2019-08-08 10:58:31 +0800

    get section pendo id

--- projects/ngx-pendo/src/lib/ngx-pendo-id.directive.ts
+++ projects/ngx-pendo/src/lib/ngx-pendo-id.directive.ts
@@ -1,19 +1,35 @@
-import { Directive, Input, HostBinding } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, HostBinding, Input, ChangeDetectorRef } from '@angular/core';
 
 @Directive({
   // tslint:disable-next-line: directive-selector
   selector: '[ngx-pendo-id]'
 })
-export class NgxPendoIdDirective {
+export class NgxPendoIdDirective implements AfterViewInit {
 
-  pendoId: string;
+  // tslint:disable-next-line: variable-name
+  private _pendoSections: string[] = [];
+
+  // tslint:disable-next-line: variable-name
+  private _pendoId: string;
 
   @Input('ngx-pendo-id')
   @HostBinding('attr.data-pendo-id')
-  get scrollbarHidden() { return this.pendoId; }
-  set scrollbarHidden(value: string) { this.pendoId = value; }
+  get pendoId() { return this._pendoSections.concat([this._pendoId]).join('.'); }
+  set pendoId(value: string) { this._pendoId = value; }
 
-  constructor() {
+  constructor(private el: ElementRef, private cdr: ChangeDetectorRef) {
+  }
+
+  ngAfterViewInit() {
+    this._pendoSections = [];
+    let ele = this.el.nativeElement as HTMLElement;
+    while (ele) {
+      if (ele.hasAttribute('ngx-pendo-section')) {
+        this._pendoSections.push(ele.getAttribute('ngx-pendo-section'));
+      }
+      ele = ele.parentElement;
+    }
+    this.cdr.detectChanges();
   }
 
 }
