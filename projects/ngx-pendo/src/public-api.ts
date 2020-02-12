1fa9d943a97ae7952d1a580689082929d66871be
Athur Ming <mingguobin@live.com>
2020-02-12 15:37:22 +0800

    feat: update angular version to 9 (#7)
    * build: update angular version to 9
    
    * ci: close google analytics
    
    * fix: report the directive

--- projects/ngx-pendo/src/public-api.ts
+++ projects/ngx-pendo/src/public-api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of ngx-pendo
  */
 
+export * from './lib/ngx-pendo-id.directive';
+export * from './lib/ngx-pendo-section.directive';
 export * from './lib/ngx-pendo.service';
 export * from './lib/ngx-pendo.component';
 export * from './lib/ngx-pendo.injectors';
