72217ea93a8f271c30012c13a9d062654e362806
Eden Wang <yociduo@vip.qq.com>
2025-06-16 19:47:38 +0800

    feat(zoneless): enable zoneless

--- projects/standalone-app/src/app/app.config.ts
+++ projects/standalone-app/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideNgxPendo } from 'ngx-pendo';
 import { routes } from './app.routes';
@@ -6,6 +6,7 @@ import { kebabCase } from './utils';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideNgxPendo({
       pendoApiKey: 'pendo-api-key',
