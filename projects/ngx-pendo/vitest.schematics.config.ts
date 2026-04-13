7469b68c2d0e5df29ccd8df2f7b75a1f8b8107f9
Eden Wang <yociduo@vip.qq.com>
2026-04-13 14:37:00 +0800

    ci: add coverage and report the result in pr

--- projects/ngx-pendo/vitest.schematics.config.ts
+++ projects/ngx-pendo/vitest.schematics.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['html', 'text-summary', 'json-summary', 'json'],
+      reportsDirectory: 'coverage/ngx-pendo-schematics'
+    },
     globals: true,
     include: ['projects/ngx-pendo/schematics/**/*.spec.ts'],
     setupFiles: ['./projects/ngx-pendo/schematics/vitest.setup.ts']
